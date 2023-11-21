84fa78819f582058284165078875a6fb7545a692
Unicornycorny <egorpisarev@hotmail.com>
2023-11-21 03:39:38 +0300

    second try

---
+++ script.js
@@ -0,0 +1,25 @@
+(function() {
+    function getLoadTime() {
+      const loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
+      return loadTime;
+    }
+    function displayStats() {
+      const loadTime = getLoadTime();
+
+      const statsContainer = document.createElement('div');
+      statsContainer.style.position = 'fixed';
+      statsContainer.style.bottom = '0';
+      statsContainer.style.left = '0';
+      statsContainer.style.backgroundColor = '#f0f0f0';
+      statsContainer.style.padding = '10px';
+      statsContainer.style.fontFamily = 'Arial, sans-serif';
+      statsContainer.innerHTML = `Страница загружена за: ${loadTime} мс`;
+
+      document.body.appendChild(statsContainer);
+    }
+  
+    window.addEventListener('load', function() {
+      displayStats();
+    });
+  })();
+  
