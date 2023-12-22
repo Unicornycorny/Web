473d40a0773149d98a13114806b10743bcd514d2
Unicornycorny <egorpisarev@hotmail.com>
2023-12-22 16:49:52 +0300

    async add

--- lab6script.js
+++ lab6script.js
@@ -1,19 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', async function () {
     const preloader = document.getElementById('preloader');
     const contentContainer = document.getElementById('content');
 
     if (navigator.onLine) {
         preloader.style.display = 'block';
 
-        fetchData()
-            .then(data => {
-                preloader.style.display = 'none';
-                renderData(data, contentContainer);
-            })
-            .catch(error => {
-                preloader.style.display = 'none';
-                handleError(error, contentContainer);
-            });
+        try {
+            const data = await fetchData();
+            preloader.style.display = 'none';
+            renderData(data, contentContainer);
+        } catch (error) {
+            preloader.style.display = 'none';
+            handleError(error, contentContainer);
+        }
     } else {
         const offlineMessage = document.createElement('div');
         offlineMessage.textContent = '⚠ Нет подключения к интернету';
@@ -22,22 +21,21 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
-function fetchData() {
+async function fetchData() {
     const randomFilter = Math.random() < 0.5 ? 100 : 200;
     const url = `https://jsonplaceholder.typicode.com/comments?postId=${randomFilter}`;
 
-    return new Promise((resolve, reject) => {
-        fetch(url)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                return response.json();
-            })
-            .then(resolve)
-            .catch(reject);
-    });
-    
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        throw error;
+    }
 }
 
 function renderData(data, contentContainer) {
